10e15c4237f2206e849f935257d0dfb3b2d3e261
flyingfish722 <flyingfish722@icloud.com>
2020-09-23 00:24:17 +0800

    test

---
+++ demos/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  demos
+//
+//  Created by 徐凯 on 2020/9/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "i am sinclair!" << std::endl;
+    std::cout << "i have build a github remote!" << std::endl;
+    return 0;
+}
